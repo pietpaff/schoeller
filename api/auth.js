8db273f438b00a93cbee3afa342d400276c72133
André Felmet <afelmet@uos.de>
2020-05-26 21:25:42 +0200

    fdsfdsfsdjfdslkfj

--- api/auth.js
+++ api/auth.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-  if (req.query.key == 'something') {
+  if (req.query.key == '!pUpsbAcke99LP') {
     res.end(`#EXTM3U\n#EXT-X-VERSION:3\n#EXT-X-MEDIA-SEQUENCE:0\n#EXT-X-ALLOW-CACHE:YES\n#EXT-X-TARGETDURATION:7\n#EXTINF:6.600000,\nhttps://schoeller.now.sh/output/aP7vLxG_460svvp9.webm_00000.ts\n#EXT-X-ENDLIST`);
   } else {
     res.status(403).end('Incorrect password.');
