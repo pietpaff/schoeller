354fa68274bda08317260f38bf333bc4c057db39
André Felmet <afelmet@uos.de>
2020-05-26 21:13:15 +0200

    fdfsdfsdfsd

--- api/auth.js
+++ api/auth.js
@@ -1,3 +1,10 @@
 module.exports = (req, res) => {
-  res.sendFile('../output/aP7vLxG_460svvp9.webm.m3u8');
+  res.end(`#EXTM3U
+#EXT-X-VERSION:3
+#EXT-X-MEDIA-SEQUENCE:0
+#EXT-X-ALLOW-CACHE:YES
+#EXT-X-TARGETDURATION:7
+#EXTINF:6.600000,
+aP7vLxG_460svvp9.webm_00000.ts
+#EXT-X-ENDLIST`);
 }
